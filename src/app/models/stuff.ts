7e6d6488ace9a4e84352f5806552f75fdb90d93c
MaximMashnev <maximmashnev345@gmail.com>
2026-01-20 14:01:46 +0300

    Началное значение даты последнего использования

--- src/app/models/stuff.ts
+++ src/app/models/stuff.ts
@@ -4,7 +4,7 @@ export class Stuff {
     public img: string = "",
     public brand: string = "",
     public name: string = "",
-    public dateLastWear: Date | string = "", //new Date(),
+    public dateLastWear: Date | string = "Отсутствует", //new Date(),
     public countsWear: number = 0,
     public size: string = "",
     public category: string = "",
